35f1c2c7e8c673aa9f99640451e0ff170e4acfc4
OhhJeet <155725808+OhhJeet@users.noreply.github.com>
2024-12-11 17:30:07 +0530

    connect.js updated

--- library-server/connect.js
+++ library-server/connect.js
@@ -3,7 +3,7 @@ const mongoose = require('mongoose');
 const connectDB = async () => {
   console.log("Hello");
   try {
-    mongoose.connect('mongodb+srv://E-library-User:tluYUYIxIXrreZ5f@loopedin.7kx04.mongodb.net/E-Library?retryWrites=true&w=majority&appName=LoopedIn')
+    mongoose.connect('mongodb+srv://jeethait2222:rUeu8gqAuSBdv0fQ@cluster0.l2jwv.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0')
         .then(res => console.log("DB CONNECTED"))
         .catch(err => console.log("DB NOT CONNECTED"));
 }
